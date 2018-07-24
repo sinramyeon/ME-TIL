689dcda3592ec6d4c0684ae1490c3c90142e9814
Ten <esistkalt@naver.com>
2018-07-24 21:57:37 +0900

    Create sumAlgotithm2.c

---
+++ Algorithm/sumAlgotithm2.c
@@ -0,0 +1,21 @@
+// 1-2+3-4....-98+99
+
+#include <stdio.h>
+
+main(){
+  int i=0, j=0;
+  while (1){
+    i++;
+    j = j+i;
+    if ( i>= 99) {
+      printf("%d", j);
+      break;    
+    }
+    else{
+      i++;
+      j = j-1;
+    }  
+  }
+
+
+}
