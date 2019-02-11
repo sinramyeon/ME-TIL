41ad6da2e3ed640a9895d1402b38f4be97ab4227
Ten <esistkalt@naver.com>
2019-02-11 20:21:24 +0100

    Update findMissingNum2.go

--- 5MonthsWarrior/findMissingNum2.go
+++ 5MonthsWarrior/findMissingNum2.go
@@ -3,8 +3,9 @@ func findMissingNum2(data []int, dataRange int) int {
   xorSum := 0
   // get the XOR of all the numbers from 1 to dataRange
   // XOR 연산 이용하기
-  
-  for i:=1; i<=dataRange; i++{
+  // XOR 연산은 두 값의 각 자릿수를 비교해,값이 0으로 같으면 0, 값이 1로 같으면 0, 다르면 1을 계산한다.
+
+  for i:=1; i< = dataRange; i++{
     xorSum ^= i
   }
   
