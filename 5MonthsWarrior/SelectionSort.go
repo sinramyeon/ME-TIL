ce5ed4e0336feb9a2900dbcb34563545db4f32a9
Ten <esistkalt@naver.com>
2019-02-28 20:49:30 +0100

    선택정렬

---
+++ 5MonthsWarrior/SelectionSort.go
@@ -0,0 +1,20 @@
+// 선택 정렬
+
+func SelectionSort(arr []int) {
+  size := len(arr)
+  var i, j, max, temp int
+  for i:=0; i< size-1; i++ {
+    max = 0
+    for j=1; j< size-1-i; j++ {
+      if arr[j] > arr[max] {
+        max = j
+      }
+    }
+    // arr[0]부터 비교해서
+    // 뭐가 더 크면 그거를 선택해서
+    
+    // 뒤에 있는 거랑 그 제일 큰걸 바꿔침
+    arr[size-1-i], arr[max] = arr[max], arr[size-1-i]
+  }
+
+}
