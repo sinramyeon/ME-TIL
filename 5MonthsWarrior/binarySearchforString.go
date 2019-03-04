990c9ae51b7ca1bc01569966fad0658048fbe33d
Ten <esistkalt@naver.com>
2019-03-04 21:20:49 +0100

    Update binarySearchforString.go

--- 5MonthsWarrior/binarySearchforString.go
+++ 5MonthsWarrior/binarySearchforString.go
@@ -36,9 +36,55 @@ func (t *StringTree) insertUtil(value string, curr *Node) *Node {
     if compare == 0 {
       curr.count++
     } else if compare == 1 {
-    
-    // todo
-      
+      curr.lChild = t.insertUtil(value, curr.lChild)
+    } else {
+      curr.rChild = t.insertUtil(value, curr.rChild)
     }
   }
+  return curr
+}
+
+func (t *StringTree) freeTree() {
+ t.root = nil
+}
+
+func (t *StringTree) Find(value string) bool {
+  ret := t.findUtil(t.root, value)
+  fmt.Println("Find ", value, "Return ", ret)
+  return ret
+}
+
+func (t *StirngTree) findUtil(curr *Node, value string) bool {
+  var compare int
+  if curr == nil {
+    return false
+  }
+  compare = strings.Compare(curr.value, value)
+  if compare == 0 {
+    return true
+  }
+  if compare == 1 {
+    return t.findUtil(curr.lChild, value)
+  }
+  return t.findUtil(curr.rChild, value)
+}
+
+func (t *StringTree) frequency(value string) int {
+      return t.frequencyUtil(t.root, value)
+}
+ 
+func (t *StringTree) frequencyUtil(curr *Node, value string) int {
+      var compare int
+      if curr == nil {
+            return 0
+      }
+      compare = strings.Compare(curr.value, value)
+ 
+      if compare == 0 {
+            return curr.count
+      }
+      if compare > 0 {
+            return t.frequencyUtil(curr.lChild, value)
+      }
+      return t.frequencyUtil(curr.rChild, value)
 }
