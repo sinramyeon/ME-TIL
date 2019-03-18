801a3c661019323af6cace6ba84556c7c5674dd4
Ten <esistkalt@naver.com>
2019-03-18 16:48:23 +0100

    스택으로 뭔갈 해결하기... InfixToPostfix

---
+++ 5MonthsWarrior/infixToPostfix.go
@@ -0,0 +1,70 @@
+// ??먼소리여 ㅆㅃ
+
+func InfixToPostfix(expn string) string {
+
+  fmt.Println(expn)
+  stk := new(Stack)
+  
+  output := ""
+  
+  for _, ch := range expn {
+    if ch <= '9' && ch > = '0' {
+      output = output + string(ch) 
+    } else {
+      switch ch {
+        case '+', '-', '*', '/', '%', '^' :
+          for stk.IsEmpty() == false && precedence(ch) <= precedence(stk.Top().(rune)) {
+            out := stk.Pop().(rune)
+            output = output + " " +string(out)
+          }
+          
+          stk.Push(ch)
+          output = output + " "
+          
+          case '(':
+            stk.Push(ch)
+          case ')' :
+            for stk.IsEmpty() == false && stk.Top() != '(' {
+              out := stk.Pop().(rune)
+              output = output + " " +string(out) + " "
+            }
+            
+            if stk.IsEmpty() == false && stk.Top() == '(' {
+              stk.Pop()
+            }
+      
+      }
+    }
+  }
+
+
+for stk.IsEmpty()== false {
+  out := stk.Pop().(rune)
+  output = coutput + string(out) + ""
+}
+
+return output
+}
+
+func precedence(x rune) int {
+      if x == '(' {
+            return (0)
+      }
+      if x == '+' || x == '-' {
+            return (1)
+      }
+      if x == '*' || x == '/' || x == '%' {
+            return (2)
+      }
+      if x == '^' {
+            return (3)
+      }
+      return (4)
+}
+ 
+func main() {
+      expn := "10+((3))*5/(16-4)"
+      value := InfixToPostfix(expn)
+      fmt.Println("Infix Expn: ", expn)
+      fmt.Println("Postfix Expn: ", value)
+}
