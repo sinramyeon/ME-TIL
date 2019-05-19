884c9d9cee53b03a443df9bd6ad79ac80f823929
Ten <esistkalt@naver.com>
2019-05-19 14:56:33 +0200

    반복문을 사용해서 1부터 n을 출력

---
+++ 5MonthsWarrior/python/Algorithm/3.py
@@ -0,0 +1,20 @@
+n = 20
+c = 1
+while(c<=n) :
+  print(c)
+  c = c+1
+  
+for x in [1,2,3] :
+  print x
+
+for x in (1,2,3) :
+  print x
+  
+  
+dic = {"key1" : 1, "key2", 2: "key3" : 3}
+for x in dic :
+  print x
+  print dic[x]
+  
+for x in `abcdefg` :
+  print x
