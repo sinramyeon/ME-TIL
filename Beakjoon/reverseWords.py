3f7a5f0d3ea71aab24d37bc869cc320a09007afb
sinramyeon <esistkalt@naver.com>
2020-06-15 11:29:23 +0900

    Update reverseWords.py

--- Beakjoon/reverseWords.py
+++ Beakjoon/reverseWords.py
@@ -1,11 +1,11 @@
 def printReverse(text) :
-    text = text.split()
+    text = text.split() ## how about use a join() and reverse() ?
     for t in text :        
         if len(t) == 1:
             print(t, end = ' ')
         else :
             length = len(t)
-            while length > 0 :
+            while length > 0 : 
                 print(t[length-1], end = '')
                 length -= 1
             print(' ', end='')
