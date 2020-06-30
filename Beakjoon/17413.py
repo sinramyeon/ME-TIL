66e094fcc0d34651244e3df85cc57621d80afb19
sinramyeon <esistkalt@naver.com>
2020-06-30 11:53:37 +0900

    Update 17413.py

--- Beakjoon/17413.py
+++ Beakjoon/17413.py
@@ -8,7 +8,7 @@ for char in string:
             result += stack.pop() #단어뒤집기~
             print("stack not empty : ", result)
         result += char
-        print(result)
+        print("stack empty :", result)
         tag = True # < 시작부터 더하다
     elif char =='>':
         result += char
@@ -21,9 +21,10 @@ for char in string:
         while not stack.empty():
             result += stack.pop()
         result += char
+        # 빈거일땐 걍 뒤집고 넣어
     else:
         stack.append(char) # 스택에 더했음
-
+        print("else ", stack);
 while not len(stack) == 0:
     result += stack.pop()
 print(result)
