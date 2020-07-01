51fc1cc70cf94c1b96f205350260ee26d50375d2
sinramyeon <esistkalt@naver.com>
2020-07-01 10:50:13 +0900

    Update 17298.py

--- Beakjoon/17298.py
+++ Beakjoon/17298.py
@@ -1,5 +1,3 @@
-import sys
-
 N = 4
 input = [3, 5, 2, 7]
 
@@ -7,15 +5,22 @@ stack = []
 result = [-1 for _ in range(N)]
 stack.append(0)
 
-print(stack, result)
+print(input, stack, result)
+print("-----------------")
 
 i = 1
 while stack and i < N:
-    while stack and input[stack[-1]] < input[i]:
-        result[stack[-1]] = input[i] # 뒤가더크면 
-        stack.pop() # -1 대신 값넣고, 스택에서 꺼내고
     
-    stack.append(i) # 스택에 값추가하고
+    while stack and input[stack[-1]] < input[i]:
+        
+        result[stack[-1]] = input[i] # 뒤가더크면
+        print("result 값추가", result)
+        
+        stack.pop() # -1 대신 값넣고, 스택에서 꺼내고
+        print("stack.pop :", stack)
+        
+    stack.append(i) # 안크면 스택에 값추가하고
+    print("stack.append :", stack)
     i += 1
 
 for i in range(N):
