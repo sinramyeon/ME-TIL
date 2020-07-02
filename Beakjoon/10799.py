1c8097abee86f48dad95278e48e950a791aa85da
sinramyeon <esistkalt@naver.com>
2020-07-02 10:34:25 +0900

    Create 10799.py

---
+++ Beakjoon/10799.py
@@ -0,0 +1,13 @@
+inputs = input()
+sumval = 0
+left = 0
+
+for i in range(len(inputs)):
+    if inputs[i] == '(':
+        left += 1
+    elif inputs[i] == ')':
+        left -= 1 # 괄호수세기
+        if inputs[i-1] == '(': #레이저
+            sumval += left
+        else :
+            sumval +=1
