34140bed1c45e4d0f84edb844d507bee8a0804db
sinramyeon <esistkalt@naver.com>
2020-08-12 08:39:00 +0200

    Update 2089.py

--- Beakjoon/2089.py
+++ Beakjoon/2089.py
@@ -5,9 +5,9 @@ res = ''
 while N: 
     if N%(-2):  # -2로나눠
         res = '1' + res
-        N = N//-2 + 1 
+        N = (N//-2) + 1 
     else: # 아닌데
         res = '0' + res
-        N //= -2
+        N = (n//-2)
         
 print(res)
